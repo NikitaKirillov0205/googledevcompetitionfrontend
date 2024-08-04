de24034d36a8dbfe30f0b4c11fb295a31c00482c
NikitaKirillov0205 <nikitakirillov0205@gmail.com>
2024-08-04 17:11:28 +0200

    line edited

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-const first = document.getElementById("scrolling-txt")
-const second = document.getElementById("scrolling-txt2")
-const third = document.getElementById("scrolling-txt3")
-const container = document.getElementById("container")
-const rect = container.getBoundingClientRect()
+const first = document.getElementById("scrolling-txt");
+const second = document.getElementById("scrolling-txt2");
+const third = document.getElementById("scrolling-txt3");
+const container = document.getElementById("container");
+const rect = container.getBoundingClientRect();
 
-const animate = (element,position) => {
-    element.style.transform = `translateX(${position}px)`
-} 
+const animate = (element, position) => {
+    element.style.transform = `translateX(${position}px)`;
+};
 
-document.addEventListener('scroll', function(e) {
-    lastKnownScrollPosition = window.scrollY;
-      
-     window.requestAnimationFrame(function() {
-       
-        animate(first,lastKnownScrollPosition*-.9)
-        animate(third,lastKnownScrollPosition -2900 *.9)
-        if (lastKnownScrollPosition<1100){
-        animate(second,(lastKnownScrollPosition-1100)*.9)}
-      });
+document.addEventListener('scroll', function (e) {
+    const lastKnownScrollPosition = window.scrollY;
+    
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        return;
+    }
+
+    window.requestAnimationFrame(function () {
+        animate(first, lastKnownScrollPosition * -0.9);
+        animate(third, lastKnownScrollPosition - 2900 * 0.9);
+        if (lastKnownScrollPosition < 1100) {
+            animate(second, (lastKnownScrollPosition - 1100) * 0.9);
+        }
+    });
 });
